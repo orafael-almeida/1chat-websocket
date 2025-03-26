de7caea20c8e6cd8c396695d25e34dbcc59eed0a
Rafael Almeida <almeidarp.dev@gmail.com>
2025-03-26 15:02:30 -0300

    feat: create Container and adjust layout responsiveness

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,15 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import Header from "@/components/Header";
 
 export const metadata: Metadata = {
   title: "1Chat",
   description: "Um chat, conversas infinitas",
 };
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
   children,
@@ -23,7 +27,15 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            {children}
+            <body
+              className={
+                inter.className +
+                "min-h-screen pt-[80px] sm:pt-[90px] md:pt-[100px]"
+              }
+            >
+              <Header />
+              {children}
+            </body>
           </ThemeProvider>
         </body>
       </html>
